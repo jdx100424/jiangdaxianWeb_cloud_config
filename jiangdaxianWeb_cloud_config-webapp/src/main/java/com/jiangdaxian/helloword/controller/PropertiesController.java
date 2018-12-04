5cc09eb776069c201c0ad1f34e638bbcede40d9b
江达贤 <jiangdaxian@ds.cn>
2018-12-04 09:42:43 +0800

    Config

---
+++ jiangdaxianWeb_cloud_config-webapp/src/main/java/com/jiangdaxian/helloword/controller/PropertiesController.java
@@ -0,0 +1,14 @@
+package com.jiangdaxian.helloword.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller()
+public class PropertiesController {
+	private static final Logger LOG = LoggerFactory.getLogger(PropertiesController.class);
+
+}
